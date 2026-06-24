e8bbce7c346b7f0c4da5ee89dc49abe1fc673147
VitorVT9 <vsilvab57@gmail.com>
2026-06-23 21:44:52 -0300

    feat: adiciona teste inicial de compração

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const form = document.getElementById('form-checagem');
 const campoA = document.getElementById('campoA');
 const campoB = document.getElementById('campoB');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let valorA = Number(campoA.value);
+    let valorB = Number(campoB.value);
+
+    if ( valorB > valorA ) {
+    console.log("Tudo certo! Formulário válido.");
+} else {
+    console.log("Erro! B precisa ser maior que A.");
+}
+})
